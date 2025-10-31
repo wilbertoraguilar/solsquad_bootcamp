bc08bff7fe492182096b5212da42dae1acf5d4dc
AlejandroLedesmaPascual <alexledesmapascual@gmail.com>
2025-10-31 11:02:17 +0000

    Problem solved: Ejercicio 1

--- src/solsquad_bootcamp/main.py
+++ src/solsquad_bootcamp/main.py
@@ -5,6 +5,7 @@ from typing import Any
 
 import typer
 import pandas as pd
+import numpy as np
 
 def read_csv_to_df(path: str) -> Any:
     if not os.path.isabs(path):
@@ -31,5 +32,8 @@ def main(path: str = "data/mock-data.csv") -> None:
     except Exception:
         typer.echo(str(df.head()))
     output = typer.prompt("Extracted file name")
-    df.to_csv("data/" + output, index=False)
+    hoy = pd.Timestamp('today').normalize()
+
+    filtered = df[ ((hoy - pd.to_datetime(df['birthdate'])) / np.timedelta64(1, 'D')) / 365.25 < 3 ]
+    filtered.to_csv("data/" + output, index=False)
     print("File saved as: data/" + output)
